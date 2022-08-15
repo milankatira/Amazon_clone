313e87274166356ca5677e9d1b1e1a8dc8ef4627
milan Katira <84834795+milankatira@users.noreply.github.com>
2022-08-15 10:01:28 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:amazon_clone/constants/global_variable.dart';
+import 'package:amazon_clone/features/auth/screen/auth_screen.dart';
+import 'package:amazon_clone/router.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+            scaffoldBackgroundColor: GlobalVariables.backgroundColor,
+            colorScheme: const ColorScheme.light(
+                primary: GlobalVariables.secondaryColor),
+            appBarTheme: const AppBarTheme(
+                elevation: 0, iconTheme: IconThemeData(color: Colors.black))),
+ 
+        onGenerateRoute: (settings) => generateRoute(settings),
+ 
+        home: Scaffold(
+            appBar: AppBar(title: const Text('mk')),
+            body: Column(
+              children: [
+                const Center(child: Text('Flutter Demo Home Page')),
+                Builder(builder: (context) {
+                  return ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, AuthScreen.routeName);
+                      },
+                      child: const Text("mk"));
+                })
+              ],
+            )));
+  }
+}
